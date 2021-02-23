3ba3ed1214f58dee44eee3a7d1465acae78e8f1a
Mariia <mshcherbina99@gmail.com>
2021-02-23 18:29:02 +0200

    added initial version of GA on python

---
+++ mariia/genetic_algorithm.py
@@ -0,0 +1,76 @@
+import random
+
+
+class GeneticAlgorithm:
+    def __init__(self, initial_population, fitness_function, crossover, mutation, mutation_prob=0.15, epochs=50, goal=1e9, repro_rate=0.5):
+        self.population = []
+        for individual in initial_population:
+            fitness = fitness_function(individual)
+            self.population.append((individual, fitness))
+        self.population_size = len(self.population)
+        self.fitness_function = fitness_function
+        self.mutation = mutation
+        self.crossover = crossover
+        self.epochs = epochs
+        self.mutation_prob = mutation_prob
+        self.goal = goal
+        self.repro_rate = repro_rate
+
+    # roulette wheel selection
+    def select(self):
+        def restore_individual(fitness):
+            for i in range(len(fitnesses)):
+                if fitnesses[i] > fitness:
+                    return self.population[i]
+            return self.population[-1]
+
+        current_fitness = 0
+        fitnesses = []
+        for individual in self.population:
+            current_fitness += individual[1]
+            fitnesses.append(current_fitness)
+
+        rand1 = random.randint(0, current_fitness - 1)
+        rand2 = random.randint(0, current_fitness - 1)
+        return restore_individual(rand1),  restore_individual(rand2)
+
+    def fit(self):
+        selections_per_epoch = int(self.population_size * self.repro_rate)
+        mutations_per_epoch = int(self.population_size * self.mutation_prob)
+        for epoch in range(self.epochs):
+            if self.reached_goal():
+                return
+
+            for selection in range(selections_per_epoch):
+                parent1, parent2 = self.select()
+                offspring = self.crossover(parent1, parent2)
+                fitness = self.fitness_function(offspring)
+                self.population.append((offspring, fitness))
+
+                for _ in range(mutations_per_epoch):
+                    individual = self.population[random.randint(0, self.population_size - 1)][0]
+                    mutant = self.mutation(individual)
+                    if mutant is not None:
+                        self.population.append((mutant, self.fitness_function(mutant)))
+
+            self.finest_survival()
+
+        return self.most_fit()[0]
+
+    def finest_survival(self):
+        sorted_population = sorted(self.population, key=lambda x: -x[1])
+        self.population = sorted_population[0:self.population_size]
+
+    def most_fit(self):
+        best = self.population[0]
+
+        for individual in self.population:
+            if individual[1] > best[1]:
+                best = individual
+
+        return best
+
+    def reached_goal(self):
+        if self.most_fit()[1] >= self.goal:
+            return True
+        return False
---
+++ mariia/main.py
@@ -0,0 +1,242 @@
+from collections import namedtuple
+from genetic_algorithm import GeneticAlgorithm
+from copy import deepcopy
+import random
+
+Gene = namedtuple('Gene', ['subject', 'group', 'teacher', 'classroom', 'pair'])
+ScheduleEntry = namedtuple('ScheduleEntry', ['subject', 'class1', 'teacher1', 'class2', 'teacher2'])
+
+DAY_PER_WEEK = 5
+CLASSES_PER_DAY = 4
+
+
+class ScheduleProblem:
+    def __init__(self):
+        self.groups_count = int(input())
+        self.teachers_count = int(input())
+        self.timeslots = DAY_PER_WEEK * CLASSES_PER_DAY
+        self.subject_count, self.lectures_count = map(int, input().split())
+        self.classroom_count, self.lecture_classroom_count = map(int, input().split())
+        self.subject_teacher_pairs = int(input())
+        self.subject_to_teacher = [[] for _ in range(self.subject_count)]
+        for i in range(self.subject_teacher_pairs):
+            subject, teacher = map(int, input().split())
+            self.subject_to_teacher[subject].append(teacher)
+
+        self.study_plan_requirements_count = int(input())
+        self.study_plan_requirements = [[0 for _ in range(self.subject_count)] for _ in range(self.groups_count)]
+        for i in range(self.study_plan_requirements_count):
+            subject, group, count = map(int, input().split())
+            self.study_plan_requirements[group][subject] = count
+
+    def is_valid(self, schedule):
+        teacher_timeslot_pairs = set()
+        classroom_timeslot_pairs = set()
+        for group in range(self.groups_count):
+            for timeslot in range(self.timeslots):
+                entry = schedule[group][timeslot]
+                if entry is None:
+                    continue
+
+                if (timeslot, entry.teacher1) in teacher_timeslot_pairs:
+                    return False
+                if (timeslot, entry.teacher2) in teacher_timeslot_pairs:
+                    return False
+
+                if (timeslot, entry.class1) in classroom_timeslot_pairs:
+                    return False
+                if (timeslot, entry.class2) in classroom_timeslot_pairs:
+                    return False
+
+                if self.is_lecture(entry.subject) and entry.class2 is not None:
+                    return False
+
+        return True
+
+    def fitness(self, schedule):
+        if not self.is_valid(schedule):
+            return -1e9
+
+        group_subject_count = [[0 for _ in range(self.subject_count)] for _ in range(self.groups_count)]
+        for group in range(self.groups_count):
+            for timeslot in range(self.timeslots):
+                if schedule[group][timeslot] is not None:
+                    group_subject_count[group][schedule[group][timeslot].subject] += 1
+
+        score = 0
+        for group in range(self.groups_count):
+            for subject in range(self.subject_count):
+                subject_score = min(self.study_plan_requirements[group][subject], group_subject_count[group][subject])
+                score += 2 * subject_score
+
+                # subject completion bonus
+                if self.study_plan_requirements[group][subject] > 1 and self.study_plan_requirements[group][subject] == group_subject_count[group][subject]:
+                    score += 1
+
+        return score
+
+    def crossover(self, parent1, parent2):
+        most_fit_offspring = None
+        max_fitness = -1e9
+
+        for group in range(self.groups_count):
+            timeslot = random.randint(0, self.timeslots - 1)
+            offspring = deepcopy(parent1[0])
+
+            for timeslots in range(timeslot + 1, self.timeslots):
+                entry = offspring[group][timeslot]
+                offspring[group][timeslot] = parent2[0][group][timeslot]
+                if not self.is_valid(offspring):
+                    offspring[group][timeslot] = entry
+
+            fitness = self.fitness(offspring)
+            if most_fit_offspring is None or fitness > max_fitness:
+                most_fit_offspring = offspring
+                max_fitness = fitness
+
+        return most_fit_offspring
+
+    def first_fit_group(self, group, schedule):
+        start_subject = random.randint(0, self.subject_count)
+        for offset in range(self.subject_count):
+            subject = (start_subject + offset) % self.subject_count
+            count = self.study_plan_requirements[group][subject]
+            timeslots = [i for i in range(self.timeslots)]
+            random.shuffle(timeslots)
+            for timeslot in timeslots:
+                if schedule[group][timeslot] is not None:
+                    continue
+
+                if self.is_lecture(subject):
+                    for classroom in range(self.lecture_classroom_count):
+                        for teacher in self.subject_to_teacher[subject]:
+                            if count == 0:
+                                break
+                            schedule[group][timeslot] = ScheduleEntry(subject, teacher, classroom, None, None)
+                            if self.is_valid(schedule):
+                                count -= 1
+                                continue
+                            else:
+                                schedule[group][timeslot] = None
+                else:
+                    for teacher1 in self.subject_to_teacher[subject]:
+                        for teacher2 in self.subject_to_teacher[subject]:
+                            if teacher2 == teacher1:
+                                continue
+
+                            for classroom1 in range(self.classroom_count):
+                                for classroom2 in range(self.classroom_count):
+                                    if classroom1 == classroom2:
+                                        continue
+
+                                    if count == 0:
+                                        break
+
+                                    schedule[group][timeslot] = ScheduleEntry(subject,
+                                                                teacher1, classroom1, teacher2, classroom2)
+                                    if self.is_valid(schedule):
+                                        count -= 1
+                                        continue
+                                    else:
+                                        schedule[group][timeslot] = None
+
+        return schedule
+
+    def gen_initial(self, n):
+        population = []
+
+        for individual in range(n):
+            basic = [[None for _ in range(self.timeslots)] for _ in range(self.groups_count)]
+            used = [False for _ in range(self.groups_count)]
+            for _ in range(self.groups_count - 2):
+                group_next = random.randint(0, self.groups_count-1)
+                while used[group_next]:
+                    group_next = random.randint(0, self.groups_count-1)
+                basic = self.first_fit_group(group_next, basic)
+                used[group_next] = True
+
+            population.append(basic)
+
+        return population
+
+    def mutation(self, schedule):
+        group = random.randint(0, self.groups_count - 1)
+        candidates = []
+
+        for timeslot in range(self.timeslots):
+            candidate = deepcopy(schedule)
+            if schedule[group][timeslot] is None:
+                continue
+            subject = schedule[group][timeslot].subject
+
+            if self.is_lecture(subject):
+                classroom = schedule[group][timeslot].class1
+                teacher = random.choice(self.subject_to_teacher[subject])
+                candidate[group][timeslot] = ScheduleEntry(subject, teacher, classroom, None, None)
+            else:
+                entry = candidate[group][timeslot]
+                flip = random.random()
+                if flip > 0.5:
+                    teacher1, class1 = entry.teacher1, entry.class1
+                    teacher2, class2 = entry.teacher2, entry.class2
+                    entry = ScheduleEntry(subject, teacher2, class2, teacher1, class1)
+                    candidate[group][timeslot] = entry
+
+                change_teacher = random.random()
+                if change_teacher > 0.5:
+                    for _ in range(5):
+                        teacher = random.choice(self.subject_to_teacher[subject])
+                        candidate[group][timeslot] = ScheduleEntry(entry.subject, teacher,
+                                                                   entry.class1, entry.teacher2, entry.class2)
+                        if self.is_valid(candidate):
+                            break
+                else:
+                    for _ in range(5):
+                        classroom = random.randint(0, self.classroom_count - 1)
+                        candidate[group][timeslot] = ScheduleEntry(entry.subject, entry.teacher1, classroom,
+                                                                   entry.teacher2, entry.class2)
+                        if self.is_valid(candidate):
+                            break
+
+            if self.is_valid(candidate):
+                candidates.append(candidate)
+
+        if len(candidates) == 0:
+            return None
+
+        return random.choice(candidates)
+
+    @staticmethod
+    def decode_timeslot(timeslot):
+        return timeslot // CLASSES_PER_DAY, timeslot % CLASSES_PER_DAY
+
+    def decode(self, schedule):
+        count = 0
+        for group in range(self.groups_count):
+            for timeslot in range(self.timeslots):
+                if schedule[group][timeslot] is None:
+                    continue
+                count += 1
+                if schedule[group][timeslot].teacher2 is not None:
+                    count += 1
+        print(count)
+        for group in range(self.groups_count):
+            for timeslot in range(self.timeslots):
+                if schedule[group][timeslot] is None:
+                    continue
+                entry = schedule[group][timeslot]
+                day, pair = self.decode_timeslot(timeslot)
+                print(entry.subject, entry.teacher1, group, entry.class1, day, pair)
+                if entry.teacher2 is not None:
+                    print(entry.subject, entry.teacher2, group, entry.class2, day, pair)
+
+    def is_lecture(self, subject):
+        return subject < self.lectures_count
+
+
+if __name__ == '__main__':
+    problem = ScheduleProblem()
+    initial_population = problem.gen_initial(50)
+    ga = GeneticAlgorithm(initial_population, problem.fitness, problem.crossover, problem.mutation, epochs=50)
+    best_schedule = ga.fit()
+    problem.decode(best_schedule)
